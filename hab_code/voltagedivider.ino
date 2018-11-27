2ab0f1fb564d802ea6194fdf5981a667e0d67e5e
Bees Knees <>
2018-11-27 16:24:19 -0500

    Almost fixed voltage divider

--- hab_code/voltagedivider.ino
+++ hab_code/voltagedivider.ino
@@ -1,16 +1,21 @@
-int voltagePin = 2;
+int voltagePin = 1;
 float voltage;
-#define R1 1000
-#define R2 10
+#define R1 10.0
+#define R2 1000.0
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  // high lows
+//  high lows
 }
 
 void loop() {
-  voltage = analogRead(voltagePin);
-  float vout = ((float) (voltage/1024.0)) * 5.0 * (1.01);
+  // put your main code here, to run repeatedly:
+  voltage = analogRead(voltagePin); // Vouts our readings
+  delay(100);
+  //Vout to Vin
+  //Vout = Vin * R2/(R1+R2)
+  float vout = (voltage/1023.0) * 5.15 * (R2+R1)/R2;
   Serial.println(vout);
   delay(1000);
 }
